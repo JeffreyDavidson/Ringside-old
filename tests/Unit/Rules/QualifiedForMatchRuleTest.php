6e940f0138b6dccd173fb9defb870dc7faa88e6f
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2018-01-17 21:23:59 -0500

    Refactored test file.

--- tests/Unit/Rules/QualifiedForMatchRuleTest.php
+++ tests/Unit/Rules/QualifiedForMatchRuleTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Feature\Unit;
 
-use App\Models\Event;
 use App\Models\Wrestler;
+use EventFactory;
 use App\Rules\QualifiedForMatch;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -16,22 +16,21 @@ class QualifiedForMatchRuleTest extends TestCase
     public function a_wrestler_with_a_hired_at_date_after_an_event_date_cannot_participate()
     {
         $wrestler = factory(Wrestler::class)->create(['hired_at' => '2017-10-10']);
-        $event = factory(Event::class)->create(['date' => '2017-10-09']);
+        $event = EventFactory::create(['date' => '2017-10-09']);
 
         $validator = new QualifiedForMatch($event->date);
 
-        $this->assertFalse($validator->passes(null , $wrestler->id));
-        $this->assertEquals('This wrestler is not qualified for the match.', $validator->message());
+        $this->assertFalse($validator->passes('hired_at', $wrestler));
     }
 
     /** @test */
     public function a_wrestler_with_a_hired_at_date_before_or_equal_to_an_event_date_can_participate()
     {
         $wrestler = factory(Wrestler::class)->create(['hired_at' => '2017-10-08']);
-        $event = factory(Event::class)->create(['date' => '2017-10-09']);
+        $event = EventFactory::create(['date' => '2017-10-09']);
 
         $validator = new QualifiedForMatch($event->date);
 
-        $this->assertTrue($validator->passes(null, $wrestler->id));
+        $this->assertTrue($validator->passes('hired_at', $wrestler));
     }
 }
