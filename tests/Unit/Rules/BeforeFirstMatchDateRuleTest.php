e44250ab20649d6d48be16b77dbf1757d5d9e2f6
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2018-01-17 21:19:28 -0500

    Refactoring of test file.

--- tests/Unit/Rules/BeforeFirstMatchDateRuleTest.php
+++ tests/Unit/Rules/BeforeFirstMatchDateRuleTest.php
@@ -3,12 +3,11 @@
 namespace Tests\Feature\Unit;
 
 use App\Models\Event;
-use App\Models\Match;
 use App\Models\Wrestler;
 use App\Rules\BeforeFirstMatchDate;
 use Tests\TestCase;
-use EventFactory;
 use MatchFactory;
+use EventFactory;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class BeforeFirstMatchDateRuleTest extends TestCase
@@ -18,15 +17,10 @@ class BeforeFirstMatchDateRuleTest extends TestCase
     /** @test */
     public function a_wrestler_with_a_match_after_they_were_hired_cannot_have_their_hired_at_date_after_their_first_match()
     {
-        // $wrestlerA = factory(Wrestler::class)->create(['name' => 'Wrestler A', 'hired_at' => '2017-10-08']);
-        // $match = MatchFactory::create([], [$wrestlerA]);
-        // $event = EventFactory::create(['date' => '2017-10-09'], [$match], 5);
-        $wrestler = factory(Wrestler::class)->create(['name' => 'Wrestler A', 'hired_at' => '2017-10-07']);
-        $event = factory(Event::class)->create(['date' => '2017-10-12']);
-        // $match = MatchFactory::create(['event_id' => $event->id], [$wrestlerA, $wrestlerB]);
-        $match = factory(Match::class)->create(['event_id' => $event->id]);
-        $match->wrestlers()->saveMany([$wrestler]);
-        // dd($match->wrestlers);
+        $wrestler = factory(Wrestler::class)->create(['hired_at' => '2017-10-07']);
+        $event = EventFactory::create(['date' => '2017-10-09']);
+        $match = MatchFactory::create(['event_id' => $event->id], [$wrestler]);
+
         $validator = new BeforeFirstMatchDate($wrestler);
 
         $this->assertFalse($validator->passes('hired_at', '2017-10-14'));
@@ -35,14 +29,11 @@ class BeforeFirstMatchDateRuleTest extends TestCase
     /** @test */
     public function a_wrestler_with_a_match_and_a_hired_at_date_before_the_first_match_can_be_updated()
     {
-        $wrestlerA = factory(Wrestler::class)->create(['hired_at' => '2017-10-07']);
-        $wrestlerB = factory(Wrestler::class)->create();
+        $wrestler = factory(Wrestler::class)->create(['hired_at' => '2017-10-07']);
         $event = factory(Event::class)->create(['date' => '2017-10-12']);
-        // $match = MatchFactory::create(['event_id' => $event->id], [$wrestlerA, $wrestlerB]);
-        $match = factory(Match::class)->create(['event_id' => $event->id]);
-        $match->wrestlers()->saveMany([$wrestlerA, $wrestlerB]);
+        $match = MatchFactory::create(['event_id' => $event->id], [$wrestler]);
 
-        $validator = new BeforeFirstMatchDate($wrestlerA);
+        $validator = new BeforeFirstMatchDate($wrestler);
 
         $this->assertTrue($validator->passes('hired_at', '2017-10-10'));
     }
