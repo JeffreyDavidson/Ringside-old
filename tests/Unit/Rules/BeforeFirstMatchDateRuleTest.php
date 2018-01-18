238cd860f0b7ac778b0426e5c4838b37b152558a
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2018-01-17 21:21:38 -0500

    Final refactoring.

--- tests/Unit/Rules/BeforeFirstMatchDateRuleTest.php
+++ tests/Unit/Rules/BeforeFirstMatchDateRuleTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature\Unit;
 
-use App\Models\Event;
 use App\Models\Wrestler;
 use App\Rules\BeforeFirstMatchDate;
 use Tests\TestCase;
@@ -30,7 +29,7 @@ class BeforeFirstMatchDateRuleTest extends TestCase
     public function a_wrestler_with_a_match_and_a_hired_at_date_before_the_first_match_can_be_updated()
     {
         $wrestler = factory(Wrestler::class)->create(['hired_at' => '2017-10-07']);
-        $event = factory(Event::class)->create(['date' => '2017-10-12']);
+        $event = EventFactory::create(['date' => '2017-10-12']);
         $match = MatchFactory::create(['event_id' => $event->id], [$wrestler]);
 
         $validator = new BeforeFirstMatchDate($wrestler);
