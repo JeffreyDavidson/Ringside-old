53320b4d115b887d5795d965252d6c087b256d79
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2018-04-28 07:21:03 -0400

    Remove datatables from system.

--- resources/assets/js/script.js
+++ resources/assets/js/script.js
@@ -1,14 +1,3 @@
-$(document).ready(function() {
-    $('.datatable').DataTable({
-        "pagingType": "full_numbers",
-        "columnDefs": [
-            { "width": "20px", "targets": 0 },
-            { "width": "150px", "targets": -1 },
-            { "targets": -1, "orderable": false }
-        ]
-    });
-} );
-
 $('.datepicker').datepicker({
     format: 'yyyy-mm-dd'
 });
