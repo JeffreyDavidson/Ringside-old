425831cdd4c4149595f8df137beb97944e8d7d8e
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2018-07-13 13:05:24 -0400

    Update.

--- tests/Unit/Models/RefereeTest.php
+++ tests/Unit/Models/RefereeTest.php
@@ -3,7 +3,7 @@
 namespace Tests\Unit\Models;
 
 use Tests\TestCase;
-use App\Models\Wrestler;
+use App\Models\Referee;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -14,11 +14,11 @@ class RefereeTest extends TestCase
     /** @test */
     public function it_can_get_referees_hired_before_a_certain_date()
     {
-        $refereeA = factory(Wrestler::class)->create(['hired_at' => Carbon::parse('2016-12-31')]);
-        $refereeB = factory(Wrestler::class)->create(['hired_at' => Carbon::parse('2014-12-31')]);
-        $refereeC = factory(Wrestler::class)->create(['hired_at' => Carbon::parse('2017-01-01')]);
+        $refereeA = factory(Referee::class)->create(['hired_at' => Carbon::parse('2016-12-31')]);
+        $refereeB = factory(Referee::class)->create(['hired_at' => Carbon::parse('2014-12-31')]);
+        $refereeC = factory(Referee::class)->create(['hired_at' => Carbon::parse('2017-01-01')]);
 
-        $hiredReferees = Wrestler::hiredBefore(Carbon::parse('2017-01-01'))->get();
+        $hiredReferees = Referee::hiredBefore(Carbon::parse('2017-01-01'))->get();
 
         $this->assertTrue($hiredReferees->contains($refereeA));
         $this->assertTrue($hiredReferees->contains($refereeB));
