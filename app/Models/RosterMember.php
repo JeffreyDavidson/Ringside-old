d3ecc8b54383430f4fa0116b6428ab4e5d0d4996
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2018-11-29 10:11:38 -0500

    Another

---
+++ app/Models/RosterMember.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Hireable;
+use App\Traits\HasStatus;
+use App\Traits\HasTitles;
+use App\Traits\HasMatches;
+use App\Traits\HasInjuries;
+use App\Traits\HasManagers;
+use App\Traits\HasRetirements;
+use App\Traits\HasSuspensions;
+use Illuminate\Database\Eloquent\Model;
+use Laracodes\Presenter\Traits\Presentable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class RosterMember extends Model
+{
+    use Hireable, HasStatus, HasTitles, HasMatches, HasInjuries, HasManagers, HasSuspensions, HasRetirements, Presentable, SoftDeletes;
+
+    protected $casts = [
+        'is_active' => 'boolean',
+        'hired_at' => 'datetime',
+    ];
+}
---
+++ app/Policies/RosterMemberPolicy.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\RosterMember;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class RosterMemberPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Checks to see if the user has permission to view list of roster members.
+     *
+     * @param  \App\Models\User  $user
+     * @return bool
+     */
+    public function index(User $user)
+    {
+        return $user->hasPermission('view-roster-members');
+    }
+
+    /**
+     * Checks to see if the user has permission to create a roster member.
+     *
+     * @param  \App\Models\User  $user
+     * @return bool
+     */
+    public function create(User $user)
+    {
+        return $user->hasPermission('create-roster-member');
+    }
+
+    /**
+     * Checks to see if the user has permission to view a roster member.
+     *
+     * @param  \App\Models\User  $user
+     * @return bool
+     */
+    public function view(User $user)
+    {
+        return $user->hasPermission('view-roster-member');
+    }
+
+    /**
+     * Checks to see if the user has permission to update a roster member.
+     *
+     * @param  \App\Models\User  $user
+     * @return bool
+     */
+    public function update(User $user)
+    {
+        return $user->hasPermission('update-roster-member');
+    }
+
+    /**
+     * Checks to see if the user has permission to delete a roster member.
+     *
+     * @param  \App\Models\User  $user
+     * @return bool
+     */
+    public function delete(User $user)
+    {
+        return $user->hasPermission('delete-roster-member');
+    }
+
+    /**
+     * Checks to see if the user has permission to activate a roster member.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Wrestler  $wrestler
+     * @return bool
+     */
+    public function activate(User $user, RosterMember $rosterMember)
+    {
+        return $user->hasPermission('activate-roster-member') && $rosterMember->hired_at->lte(today());
+    }
+
+    /**
+     * Checks to see if the user has permission to deactivate a roster member.
+     *
+     * @param  \App\Models\User  $user
+     * @return bool
+     */
+    public function deactivate(User $user)
+    {
+        return $user->hasPermission('deactivate-roster-member');
+    }
+
+    /**
+     * Checks to see if the user has permission to retire a roster member.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Wrestler  $wrestler
+     * @return bool
+     */
+    public function retire(User $user, RosterMember $rosterMember)
+    {
+        return $user->hasPermission('retire-roster-member') && $rosterMember->hired_at->lte(today());
+    }
+
+    /**
+     * Checks to see if the user has permission to retire a roster member.
+     *
+     * @param  \App\Models\User  $user
+     * @return bool
+     */
+    public function unretire(User $user)
+    {
+        return $user->hasPermission('unretire-roster-member');
+    }
+}
